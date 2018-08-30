6a64499385327862421735c79aca3bbb6de27d69
rafabastos <rbastosclemente@gmail.com>
2018-08-30 01:05:20 -0400

    se agrega entidades base

---
+++ sigel.Module/BusinessObjects/BaseCode/Ciudad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace sigel.Module.BusinessObjects.ORMDataModel1
+{
+
+    public partial class Ciudad
+    {
+        public Ciudad(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ sigel.Module/BusinessObjects/GestionesCode/GestionMonsanto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace sigel.Module.BusinessObjects.Gestiones
+{
+
+    public partial class GestionMonsanto
+    {
+        public GestionMonsanto(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
