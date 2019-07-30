91d24344a2c3bb5886ef00fc0eae19bb6a7cf91e
rafael bastos <rafael.bastos@softec.com.py>
2019-07-30 14:42:31 -0400

    ABMs para gestion

---
+++ sigel.Module/BusinessObjects/BaseCode/Socio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace sigel.Module.BusinessObjects.ORMDataModel1
+{
+
+    public partial class Socio
+    {
+        public Socio(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
